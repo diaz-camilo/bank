8cc9e896079f7e7b1621f114eb3780cd2ea5428f
Camilo <cajadi86@hotmail.com>
2021-08-12 20:43:43 +1000

    Signup, login, logout and authorization  implemented

---
+++ WebBanking/Repository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using WebBanking.Models;
+using WebBanking.ViewModels;
+
+namespace WebBanking.Repository
+{
+    public interface IUserRepository
+    {
+        Task<IdentityResult> CreateUserAsync(SignupUser signupUser);
+        Task<SignInResult> LoginUserAsync(LoginViewModel login);
+        Task LogoutUserAsync();
+    }
+}
---
+++ WebBanking/Repository/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using WebBanking.Models;
+using WebBanking.ViewModels;
+
+namespace WebBanking.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public UserRepository(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+        public async Task<IdentityResult> CreateUserAsync(SignupUser signupUser)
+        {
+            
+
+            var user = new AppUser()
+            {
+                //Id = signupUser.LoginID,
+                UserName = signupUser.LoginID.ToString(),
+                CustomerID = signupUser.CustomerID,
+            };
+
+            var result = await _userManager.CreateAsync(user,signupUser.password);
+            return result;
+        }
+
+        public async Task<SignInResult> LoginUserAsync(LoginViewModel login)
+        {
+            var result = await _signInManager.PasswordSignInAsync(login.LoginID, login.Password, true, false);
+            
+            return result;
+        }
+
+        public async Task LogoutUserAsync()
+        {
+            await _signInManager.SignOutAsync();
+        }
+
+    }
+}
